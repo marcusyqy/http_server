83d221100fba9218921e9d6de89a8cef8f46e8be
Marcus Yong <marcusyqy@gmail.com>
2024-09-27 01:40:15 -0400

    Need to decode http protocol next

--- src/main.c
+++ src/main.c
@@ -4,9 +4,9 @@
 #include "os/net.c"
 
 #define PORT "8000"
+#define DEFAULT_BUFLEN 512
 
 int main(void) {
-  int return_code = 0;
   printf("hello world\n");
 
   // @TODO: move this back into somewhere the os windows will use.
@@ -30,21 +30,81 @@ int main(void) {
 
   // Resolve the local address and port to be used by the server
   int call_result = getaddrinfo(NULL, PORT, &hints, &result);
-  if (call_result != 0) {
+  if(call_result != 0) {
     printf("getaddrinfo failed: %d\n", call_result);
-    return_code = 1;
-    goto end;
+    os_win32_net_exit();
+    return 1;
   }
 
   SOCKET listen_socket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
   if(listen_socket == INVALID_SOCKET) {
     printf("Error at socket(): %d\n", WSAGetLastError());
-    return_code = 1;
-    goto end;
+    freeaddrinfo(result);
+    os_win32_net_exit();
+    return 1;
+  }
+
+  // Setup the TCP listening socket
+  call_result = bind(listen_socket, result->ai_addr, (int)result->ai_addrlen);
+  if(call_result == SOCKET_ERROR) {
+    printf("bind failed with error: %d\n", WSAGetLastError());
+    freeaddrinfo(result);
+    closesocket(listen_socket);
+    os_win32_net_exit();
+    return 1;
+  }
+
+  // we don't need this anymore once it is binded.
+  freeaddrinfo(result);
+
+  while(listen(listen_socket, SOMAXCONN) != SOCKET_ERROR) {
+    // accept a connection here.
+    SOCKET client_socket = accept(listen_socket, NULL, NULL);
+    if(client_socket == INVALID_SOCKET) {
+      continue;
+    }
+
+
+    char recv_buffer[DEFAULT_BUFLEN];
+    int recv_buflen = DEFAULT_BUFLEN - 1;
+
+    int recv_result = 0;
+    for(;;) {
+      recv_result = recv(client_socket, recv_buffer, recv_buflen, 0);
+      if(recv_result < 0) {
+        printf("`recv` failed with %d\n", WSAGetLastError());
+        break;
+      }
+
+      if(recv_result == 0) {
+        break;
+      }
+
+      recv_buffer[recv_result] = '\0';
+      printf("Bytes received: %d\n", recv_result);
+      printf("Received bytes: %s\n", recv_buffer);
+
+      /*
+      // Echo the buffer back to the sender
+      int send_result = send(client_socket, recv_buffer, recv_result, 0);
+      if (send_result == SOCKET_ERROR) {
+      printf("send failed: %d\n", WSAGetLastError());
+      closesocket(client_socket);
+      WSACleanup();
+      return 1;
+      }
+      printf("Bytes sent: %d\n", send_result);
+      */
+    }
+    printf("Connection closing\n");
+    closesocket(client_socket);
+    break;
   }
 
 
-end:
+
+
+  closesocket(listen_socket);
   os_win32_net_exit();
-  return return_code;
+  return 0;
 }
